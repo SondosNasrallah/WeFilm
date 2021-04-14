a914bd7bf83e649600090a59134dc4dcd604dcef
SondosNa <>
2021-04-14 20:44:24 +0000

    create

---
+++ app.js
@@ -0,0 +1,19 @@
+alert ('welcome to my web');
+
+
+
+
+var horrorMo=prompt('grading from 1-10 how much do you like horror movies?')
+if(horrorMo >=6){
+  document.write('<img src="https://live.staticflickr.com/6108/6277596671_4588e91e20_b.jpg">')
+}
+
+var yourAge=prompt('please enter your age');
+
+if(yourAge >=20){
+alert ('our content is recommended, Enjoy your film')
+
+}else{
+  alert ('our content is sensitive and not allowed for you age')
+}
+
